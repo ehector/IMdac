b9f8d0c72e0667829fd4f80d25d6d95ac74934c0
ehector <ehector@ncsu.edu>
2024-09-01 09:00:50 -0400

    Add files via upload

--- bernoulli/bernoulli-settingII-ecdf-20240828.R
+++ bernoulli/bernoulli-settingII-ecdf-20240828.R
@@ -60,13 +60,73 @@ naive_IM <- pl.naive(theta=theta_vec, theta_MLE=theta_MLE, J_ni=J_ni, p=p)
 #############################################################################
 
 # Middle ground IM
-theta_dag <- as.matrix(expand.grid(
-  c(meta_estimate[1]-sqrt(diag(solve(s.J)))[1], meta_estimate[1], meta_estimate[1]+sqrt(diag(solve(s.J)))[1]),
-  c(meta_estimate[2]-sqrt(diag(solve(s.J)))[2], meta_estimate[2], meta_estimate[2]+sqrt(diag(solve(s.J)))[2])))
+theta_dag <- matrix(meta_estimate, nrow=1)
 middle_IM <- logistic_pl_mid_P(theta=theta_vec, theta_dag=theta_dag, theta_MLE=theta_MLE, k=k, J_ni=J_ni, n_i,
                              covariates=covariates, M=50000)
 
-save.image("binomial-settingII-ecdf.RData")
+save.image("bernoulli-onedag-ecdf.RData")
+
+# dat_optimal_IM <- data.frame(theta_1=theta_vec[,1], theta_2=theta_vec[,2], type="oracle", contour=optimal_IM)
+dat_difference_IM <- data.frame(theta_1=theta_vec[,1], theta_2=theta_vec[,2], type="difference", contour=middle_IM-naive_IM)
+dat_naive_IM <- data.frame(theta_1=theta_vec[,1], theta_2=theta_vec[,2], type="meta-analysis", contour=naive_IM)
+dat_middle_IM <- data.frame(theta_1=theta_vec[,1], theta_2=theta_vec[,2], type="equilibrated", contour=middle_IM)
+
+# dat_IM <- as.data.frame(rbind(dat_optimal_IM, dat_naive_IM, dat_middle_IM))
+# dat_IM$type <- factor(dat_IM$type, levels=c("oracle","meta-analysis","equilibrated"), labels=c("oracle","meta-analysis","equilibrated"))
+dat_IM <- as.data.frame(rbind(dat_difference_IM, dat_naive_IM, dat_middle_IM))
+dat_IM$type <- factor(dat_IM$type, levels=c("meta-analysis","equilibrated","difference"), labels=c("meta-analysis","equilibrated","difference"))
+dat_IM$contour[which.max(dat_IM$contour)] <- round(dat_IM$contour[which.max(dat_IM$contour)],2)
+dat_IM$contour[dat_IM$type!="difference"][which.min(dat_IM$contour[dat_IM$type!="difference"])] <- 
+  round(dat_IM$contour[dat_IM$type!="difference"][which.min(dat_IM$contour[dat_IM$type!="difference"])],1)
+
+plot_all <-
+  ggplot(dat_IM[dat_IM$type!="difference",], aes(theta_1, theta_2)) + geom_raster(hjust = 0, vjust = 0, aes(fill = contour)) +
+  scale_fill_continuous(type = "viridis", guide=guide_colourbar(title.position="top")) +
+  xlab(expression(theta[1])) + ylab(expression(theta[2])) +
+  theme_bw() + theme(axis.title.y=element_text(size=18), axis.title.x=element_text(size=18), legend.title=element_text(size=18),
+                     axis.text.y=element_text(size=18), axis.text.x=element_text(size=18),
+                     legend.text=element_text(size=18), legend.position="bottom", legend.box = "horizontal", legend.key.width=unit(2,"cm"),
+                     strip.background = element_blank(), strip.text.x = element_text(size=18), legend.title.align=0.5) +
+  facet_wrap(~type, nrow=1, ncol=2)
+plot_naive <-
+  ggplot(dat_IM[dat_IM$type=="meta-analysis",], aes(theta_1, theta_2)) + geom_raster(hjust = 0, vjust = 0, aes(fill = contour)) +
+  scale_fill_continuous(type = "viridis") +
+  xlab("") + ylab(expression(theta[2])) + ggtitle("meta-analysis") +
+  theme_bw() + theme(axis.title.y=element_text(size=18), axis.title.x=element_text(size=18), legend.title=element_text(size=18),
+                     axis.text.y=element_text(size=18), axis.text.x=element_text(size=18),
+                     legend.text=element_text(size=18), plot.title = element_text(size=18, hjust = 0.5))
+plot_middle <-
+  ggplot(dat_IM[dat_IM$type=="equilibrated",], aes(theta_1, theta_2)) + geom_raster(hjust = 0, vjust = 0, aes(fill = contour)) +
+  scale_fill_continuous(type = "viridis") +
+  xlab(expression(theta[1])) + ylab("") + ggtitle("equilibrated") +
+  theme_bw() + theme(axis.title.y=element_blank(), axis.title.x=element_text(size=18), legend.title=element_text(size=18),
+                     axis.text.y=element_blank(), axis.ticks.y=element_blank(), axis.text.x=element_text(size=18),
+                     legend.text=element_text(size=18), plot.title=element_text(size=18, hjust = 0.5))
+plot_difference <-
+  ggplot(dat_IM[dat_IM$type=="difference",], aes(theta_1, theta_2)) + geom_raster(hjust = 0, vjust = 0, aes(fill = contour)) +
+  scale_fill_continuous(type = "viridis", option="inferno", guide=guide_colourbar(title.position="top")) +
+  xlab("") + ylab("") + ggtitle("difference") + labs(fill = "difference") +
+  theme_bw() + theme(axis.title.y=element_blank(), axis.title.x=element_text(size=18), legend.title=element_text(size=18),
+                     axis.text.y=element_blank(), axis.ticks.y=element_blank(), axis.text.x=element_text(size=18),
+                     legend.key.width=unit(2,"cm"), legend.text=element_text(size=18), legend.position="bottom", legend.box = "horizontal",
+                     plot.title = element_text(size=18, hjust = 0.5), legend.title.align=0.5)
+
+legend <- cowplot::get_plot_component(plot_all, 'guide-box-bottom', return_all = TRUE)
+legend_diff <- cowplot::get_plot_component(plot_difference, 'guide-box-bottom', return_all = TRUE)
+plots_no_legend <- cowplot::plot_grid(
+  plot_naive + theme(legend.position="none", plot.margin=margin(5,10,-110,0)),
+  plot_middle + theme(legend.position="none", plot.margin=margin(5,10,-113,-10)),
+  plot_difference + theme(legend.position="none", plot.margin=margin(5,10,-110,-10)),
+  rel_heights=c(1,1,1), rel_widths=c(1.15,1,1), 
+  nrow=1, ncol=3)
+legends <- cowplot::plot_grid(legend, 
+                              legend_diff, 
+                              ncol=2, nrow=1, rel_heights=c(1,1), rel_widths=c(2,1))
+plots_with_legend <- cowplot::plot_grid(plots_no_legend, legends, ncol=1, nrow=2)
+
+pdf("/Users/ehector/Dropbox/Projects/IMfusion/paper/figures_meta/logistic-contours.pdf", height=8, width=15)
+plots_with_legend
+dev.off()
 
 #############################################################################
 ### Empirical CDF of the middle ground contour evaluated at theta_0
@@ -102,7 +162,7 @@ for(sim in 1:nsim){
 
 save.image("bernoulli-settingII-ecdf.RData")
 
-pdf("/Users/ehector/Dropbox/Projects/IMfusion/paper/figures_meta/ecdf_bernoulli_settingII.pdf", height=5, width=5)
+pdf("/Users/ehector/Dropbox/Projects/IMfusion/paper/figures_meta/ecdf_logistic.pdf", height=5, width=5)
 plot(seq(0,1,length=100), sapply(seq(0,1,length=100), function(t) mean(samples<=t)),
      type="l", ylab="CDF", xlab=expression(alpha))
 lines(seq(0,1,length=100), sapply(seq(0,1,length=100), function(t) mean(naive_samples<=t)), col="red")
